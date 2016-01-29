1fb3970323405be069183fe12c9bcbdcd824a9a7
Michael Reynolds <michael4reynolds@gmail.com>
2016-01-29 11:27:13 -0500

    add release year and stars to movies

--- movies/media.py
+++ movies/media.py
@@ -5,13 +5,15 @@ class Movie:
     """Implements a container for movie data"""
 
     def __init__(self, movie_title, movie_storyline, poster_image,
-                 trailer_youtube):
+                 trailer_youtube, release_year, stars):
         """Assign movie information from parameters"""
 
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+        self.release_year = release_year
+        self.stars = stars
 
     def show_trailer(self):
         """Open a new browser window and show the youtube
--- movies/my_entertainment.py
+++ movies/my_entertainment.py
@@ -8,37 +8,49 @@ shawshank_remdemption = media.Movie(
         "Framed for murder, a man gains admiration in prison for his sense{0}"
         " of hope",
         "http://image.tmdb.org/t/p/w342/9O7gLzmreU0nGkIB6K3BsJbzvNv.jpg",
-        "https://www.youtube.com/watch?v=6hB3S9bIaco")
+        "https://www.youtube.com/watch?v=6hB3S9bIaco",
+        1994,
+        "Tim Robbins, Morgan Freeman, Bob Gunton")
 godfather = media.Movie(
         "The Godfather",
         "The aging patriarch of organized crime dynasty transfer control of{0}"
         " his empire to reluctant son",
         "http://image.tmdb.org/t/p/w342/d4KNaTrltq6bpkFS01pYtyXa09m.jpg",
-        "https://www.youtube.com/watch?v=w0VGcWHkNeA")
+        "https://www.youtube.com/watch?v=w0VGcWHkNeA",
+        1972,
+        "Marlon Brando, Al Pacino, James Caan")
 cuckoos_nest = media.Movie(
         "One Flew Over the Cuckoos Nest",
         "mental patient inspires his fellow patients to rebel against the{0}"
         " authoritarian rule",
         "http://image.tmdb.org/t/p/w342/srr59GKJdDXPwnWlew9NoYfOvYV.jpg",
-        "https://www.youtube.com/watch?v=2WSyJgydTsA")
+        "https://www.youtube.com/watch?v=2WSyJgydTsA",
+        1975,
+        "Jack Nicholson, Louise Fletcher, Michael Berryman")
 empire_strikes_back = media.Movie(
         "Star Wars: Episode V - The Empire Strikes Back",
         "Luke Skywalker learns the ways of the Jedi from master Yoda to{0}"
         " fight the evil empire",
         "http://image.tmdb.org/t/p/w342/6u1fYtxG5eqjhtCPDx04pJphQRW.jpg",
-        "https://www.youtube.com/watch?v=96v4XraJEPI")
+        "https://www.youtube.com/watch?v=96v4XraJEPI",
+        1980,
+        "Mark Hamill, Harrison Ford, Carrie Fisher ")
 matrix = media.Movie(
         "The Matrix",
         "hacker learns the truth about his reality and his role in the war{0}"
         " against its controllers",
         "http://image.tmdb.org/t/p/w342/ZPMhHXEhYB33YoTZZNNmezth0V.jpg",
-        "https://www.youtube.com/watch?v=Q8g9zL-JL8E")
+        "https://www.youtube.com/watch?v=Q8g9zL-JL8E",
+        1999,
+        "Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss")
 spirited_away = media.Movie(
         "Spirited Away",
         "a young girl wanders away from her parents to a world with strange{0}"
         " monster-like animals",
         "http://image.tmdb.org/t/p/w342/dL11DBPcRhWWnJcFXl9A07MrqTI.jpg",
-        "https://www.youtube.com/watch?v=6az9wGfeSgM")
+        "https://www.youtube.com/watch?v=6az9wGfeSgM",
+        2001,
+        "Daveigh Chase, Suzanne Pleshette, Miyu Irino")
 
 # Store movies in an array to pass to outside function
 movies = [spirited_away, matrix, shawshank_remdemption, godfather,
